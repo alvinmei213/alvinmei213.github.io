94c058c7b8e7c2781662664414b5b6afd13d7421
Alvin Mei <alvinkmei@gmail.com>
2022-09-13 23:10:40 -0400

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template lang="pug">
-header
-  .wrapper
-    nav
-      RouterLink(to="/") Home
-      RouterLink(to="/about") About
-RouterView
+
 </template>
 
 <style scoped>
